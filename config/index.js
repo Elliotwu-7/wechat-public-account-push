03ad6825b00437e5513948f0af3e885b7fb3beb7
Elliotwu-7 <112760141+Elliotwu-7@users.noreply.github.com>
2022-09-03 20:04:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,76 +1,33 @@
 export const config = {
-  /**
-   * 公众号配置
-   */
 
-  // 公众号APP_ID
   APP_ID: "wx8e499be176cb2b20",
 
-  // 公众号APP_SECRET
   APP_SECRET: "473a867e2bded4e03f1ec9ce235db8b5",
 
   USERS: [
     {
-      // 想要发送的人的名字
       name: "老婆",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "obqOs6fe5X7aFKKJ5BVyht6m_RRQ",
-      // 你想对他发送的模板消息的模板ID
       useTemplateId: "EvvjZ3_CbwNVZEEH8XNciKkiYIEuV3_In-WvqbaxZ_A",
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://weathernew.pae.baidu.com/weathernew/pc?query=青海西宁天气&srcid=4982"
     },     
-  /**
-   * 信息配置
-   */
-
-  /** 天气相关 */
-
-  // 所在省份
+    
   PROVINCE: "青海",
-  // 所在城市
   CITY: "西宁",
 
-  /** 重要节日相关 */
-
-  /**
-   * 重要节日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
-   * tpye必须填！ 只能 “生日” 和 “节日” 二选一!
-   * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
-   * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
-   */
   FESTIVALS: [
     {"type": "生日", "name": "老婆", "year": "2001", "date": "04-29"},
     {"type": "生日", "name": "吴岩", "year": "2000", "date": "07-07"},
     {"type": "节日", "name": "在一起纪念日", "year": "2019", "date": "07-18"},
   ],
 
-  /**
-   * 限制重要节日的展示条目, 需要填写数字; 
-   * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
-   * 如果为0, 则默认展示全部
-   */
   FESTIVALS_LIMIT: 3,
 
-  /** 日期相关 */
-
-  /** 你现在可以随心增加你认为的所有的需要纪念的日子啦！
-    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-    * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }} 
-    * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
     {"keyword": "love_day", date: "2019-07-18"},
 
-
-  /** 插槽 */
-
-  /** 你可以在这里写超多的你想显示的内容了！
-    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-    * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }} 
-    * */
   SLOT_LIST: [
-    // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
       "因为太喜欢你，所以看谁都像是情敌。",
       "申请成为你爱里的永久居民。",
@@ -79,10 +36,6 @@ export const config = {
     ]},
   ],
 
-  /** 每日一言 */
-
-  // 好文节选的内容类型
-  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
   LITERARY_PREFERENCE: ""
 
 
